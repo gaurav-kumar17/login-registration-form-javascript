8ecf7e53ff95bed328a913f0f8584360eb28137d
gaurav-kumar17 <gk170106@gmail.com>
2022-08-17 19:29:04 +0530

    added javascript file and updated rest files

---
+++ script.js
@@ -0,0 +1,40 @@
+let loginForm=document.getElementById("loginForm");
+let registerForm=document.getElementById("registerForm");
+
+let loginBtn=document.getElementById("loginToggleBtn");
+
+let registerBtn=document.getElementById("registerToggleBtn");
+
+let formBox=document.getElementById("mainBox");
+
+loginBtn.addEventListener("click",function(){
+
+    loginForm.style.display="block";
+    registerForm.style.display="none";
+
+    loginBtn.classList.add("active");
+    registerBtn.classList.remove("active");
+})
+
+registerBtn.addEventListener("click",function(){
+
+    loginForm.style.display="none";
+    registerForm.style.display="block";
+
+    loginBtn.classList.remove("active");
+    registerBtn.classList.add("active");
+})
+
+// loginForm.addEventListener("submit",loginData);
+  
+// function loginData(e){
+//   e.preventDefault();
+
+//   let email = document.forms["loginForm"]["email"].value;
+  
+//   let password = document.forms["loginForm"]["password"].value;
+//   var data = JSON.parse(localStorage.getItem("users"));
+
+
+//   console.log(data);
+// }
