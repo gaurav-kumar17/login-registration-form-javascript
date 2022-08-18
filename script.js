7360395d925c83b887e0b3dd02bc64bafa2e7156
gaurav-kumar17 <gk170106@gmail.com>
2022-08-18 15:44:50 +0530

    updated html and javascript file

--- script.js
+++ script.js
@@ -26,13 +26,77 @@ registerBtn.addEventListener("click",function(){
 
 
 loginForm.addEventListener("submit",loginData);
-  
 function loginData(e){
 
-  e.preventDefault();
-  let userName=document.forms["loginForm"]["username"].value;
-  let password = document.forms["loginForm"]["password"].value;
-//   var data = JSON.parse(localStorage.getItem("users"));
-
-  console.log(userName,password);
+    e.preventDefault();
+    let email=document.forms["loginForm"]["email"].value;
+    let password = document.forms["loginForm"]["password"].value;
+    var data = JSON.parse(localStorage.getItem("users"));
+    if(data==null || data.length==0)
+    {
+        alert("No users registered");
+        
+    }
+    else{
+       for(var i=0;i<data.length;i++)
+       {
+        if(email==data[i].email && password==data[i].password)
+        {
+            alert("Login Successfully");
+            break;
+        }
+        else{
+            alert("Invalid Email or Password");
+            break;
+        }
+       }
+    }
 }
+
+
+registerForm.addEventListener("submit",registerData);
+function registerData(e)
+{
+    e.preventDefault();
+    let name=document.forms["registerForm"]["name"].value;
+    let email=document.forms["registerForm"]["email"].value;
+    let password = document.forms["registerForm"]["password"].value;
+
+    const user={
+        name:name,
+        email:email,
+        password:password,
+    };
+
+    let data=JSON.parse(localStorage.getItem("users"));
+    console.log(data);
+    
+    if(data==null || data.length<=0)
+    {
+        data=[];
+        
+    }
+    else{
+        var check=false;
+        for(let i=0;i<data.length;i++)
+        {
+            if(data[i].email==email)
+            {
+                check=true;
+                break;
+            }
+        }
+      
+    }
+
+    if(check==true)
+    {
+        alert("Email already Exist");
+    }
+    else{
+            
+        data.push(user);
+        localStorage.setItem("users",JSON.stringify(data));
+        alert("Registered Successfully");
+    }
+}   
